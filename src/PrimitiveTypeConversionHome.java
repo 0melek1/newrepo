b0464cec4f08bc6c13b4a8e5103513a47f2cd0fe
Melek <142055825+0melek1@users.noreply.github.com>
2024-04-28 21:57:28 +0400

    initial

---
+++ src/PrimitiveTypeConversionHome.java
@@ -0,0 +1,32 @@
+public class PrimitiveTypeConversionHome {
+    public static void main(String[] args){
+        int i = 55;
+        byte b = (byte)i;
+        System.out.println(b);
+
+        short s = 636;
+        byte b = (byte)s;
+        System.out.println(b);
+
+        long l = 458 ;
+        short s = (short)l;
+        System.out.println(s);
+
+        long l = 92523635483l;
+        int i = (int)l;
+        System.out.println(i);
+
+        double d = 65787.3 ;
+        int i = (int)d;
+        System.out.println(i);
+
+        char c = 'q';
+        int i = (int)c;
+        System.out.println(i);
+
+        int i = 266;
+        char c = (char)i;
+        System.out.println(c);
+
+    }
+}
