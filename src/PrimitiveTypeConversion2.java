72e1d915e5926cee53f57a569cfbd410aae0015a
Melek <142055825+0melek1@users.noreply.github.com>
2024-05-02 06:01:39 +0400

    initial

--- src/PrimitiveTypeConversion2.java
+++ src/PrimitiveTypeConversion2.java
@@ -1,7 +1,15 @@
-public class PrimitiveTypeConversion2 {
+/*public class PrimitiveTypeConversion2 {
     public static void main(String[]args){
         for( int x=15; x<40 ; x++){
             System.out.println("15 ve 40 araliginda tam ededler:" + x);
         }
     }
-}
+}*/
+
+ public class PrimitiveTypeConversion2{
+     public static void main(String[]args){
+         for ( int x = 21; x<=50 ; x=x+2){
+         System.out.println(" 20 ve 50 araliginda tek ededler:" + x);
+         }
+     }
+ }
