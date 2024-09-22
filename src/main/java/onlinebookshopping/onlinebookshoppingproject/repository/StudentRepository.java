eddd783ea6f9d1a4ea02464e91101ebcaab9de05
Melek <142055825+0melek1@users.noreply.github.com>
2024-09-22 15:51:35 +0400

    student repository class created

---
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package onlinebookshopping.onlinebookshoppingproject.repository;
+
+import onlinebookshopping.onlinebookshoppingproject.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
