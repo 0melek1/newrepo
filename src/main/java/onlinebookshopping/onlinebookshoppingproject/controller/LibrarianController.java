fd31242cfb046ed409536b532e48870d2d9e39a9
Melek <142055825+0melek1@users.noreply.github.com>
2024-09-22 22:31:04 +0400

    Kitabxanaci Qeydiyyat API

--- src/main/java/onlinebookshopping/onlinebookshoppingproject/controller/LibrarianController.java
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/controller/LibrarianController.java
@@ -1,7 +1,11 @@
 package onlinebookshopping.onlinebookshoppingproject.controller;
 
+import onlinebookshopping.onlinebookshoppingproject.model.Librarian;
 import onlinebookshopping.onlinebookshoppingproject.repository.LibrarianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,11 @@ public class LibrarianController {
     @Autowired
     private LibrarianRepository librarianRepository;
 
+    @PostMapping("/register")
+    public ResponseEntity<String> registerLibrarian(@RequestBody Librarian librarian) {
+        librarianRepository.save(librarian);
+        return ResponseEntity.ok("Kitabxanaçı uğurla qeydiyyatdan keçdi!");
 
 
+    }
 }
