592c56a2213861a3e721995b68bed675fab8e222
Melek <142055825+0melek1@users.noreply.github.com>
2024-09-22 11:11:14 +0400

    project created

---
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/OnlinebookshoppingprojectApplication.java
@@ -0,0 +1,13 @@
+package onlinebookshopping.onlinebookshoppingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookshoppingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookshoppingprojectApplication.class, args);
+	}
+
+}
