4ac78fc3f5c096c941aa161ae55916ea86fe2337
Melek <142055825+0melek1@users.noreply.github.com>
2024-10-14 12:33:11 +0400

    scheduler config updated

--- src/main/java/onlinebookshopping/onlinebookshoppingproject/config/MyConfig.java
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/config/MyConfig.java
@@ -1,5 +1,18 @@
 package onlinebookshopping.onlinebookshoppingproject.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyConfig {
+    @Bean
+    public ModelMapper mapper() {
+        ModelMapper m = new ModelMapper();
+        return m;
+    }
+
+
+
 
 }
--- src/main/java/onlinebookshopping/onlinebookshoppingproject/config/SchedulerConfig.java
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/config/SchedulerConfig.java
@@ -1,6 +1,20 @@
 package onlinebookshopping.onlinebookshoppingproject.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@Configuration
 public class SchedulerConfig {
+
+    public class SchedulerConfig {
+
+        @Scheduled(fixedDelay = 2000, initialDelay = 100_000)
+        public void metod1() {
+            System.out.println("scheduler calisir");
+        }
 
 
 }
