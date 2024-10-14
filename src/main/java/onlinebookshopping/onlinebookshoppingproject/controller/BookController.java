00486470662ad305fe23ea01b0a3e85a9c49cc93
Melek <142055825+0melek1@users.noreply.github.com>
2024-10-14 12:27:02 +0400

    book controller updated

--- src/main/java/onlinebookshopping/onlinebookshoppingproject/controller/BookController.java
+++ src/main/java/onlinebookshopping/onlinebookshoppingproject/controller/BookController.java
@@ -1,4 +1,32 @@
 package onlinebookshopping.onlinebookshoppingproject.controller;
 
+
+import onlinebookshopping.onlinebookshoppingproject.model.Book;
+import onlinebookshopping.onlinebookshoppingproject.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String keyword) {
+        return bookService.searchBooks(keyword);
+    }
 }
