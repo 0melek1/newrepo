249fa65e01ba331e7bd52a3a37eaae4d1e9303e8
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-12 22:53:08 +0400

    user repo interface created

--- end-project/end-project/src/test/java/java16/end_project/Transaction.java
+++ end-project/end-project/src/test/java/java16/end_project/Transaction.java
@@ -1,4 +1,25 @@
 package java16.end_project;
 
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String category; // Gəlir və ya xərc kateqoriyası
+    private BigDecimal amount;
+    private LocalDate date;
 }
---
+++ end-project/end-project/src/test/java/java16/end_project/repo/UserRepo.java
@@ -0,0 +1,4 @@
+package java16.end_project.repo;
+
+public interface UserRepo {
+}
