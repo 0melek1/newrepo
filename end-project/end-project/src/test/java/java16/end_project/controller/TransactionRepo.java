a62d7e5d7c3069b833891a578cfbc5275c86d4bd
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-12 23:11:16 +0400

    transaction controller class created

--- end-project/end-project/src/test/java/java16/end_project/controller/TransactionRepo.java
+++ end-project/end-project/src/test/java/java16/end_project/controller/TransactionRepo.java
@@ -1,6 +1,29 @@
 package java16.end_project.controller;
 
+import java16.end_project.Transaction;
+import java16.end_project.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/transactions")
+
 public class TransactionRepo {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping("/balance/{userId}")
+    public BigDecimal getUserBalance(@PathVariable Long userId) {
+        return transactionService.getUserBalance(userId);
+    }
+
+    @PostMapping("/add")
+    public Transaction addTransaction(@RequestBody Transaction transaction) {
+        return transactionService.saveTransaction(transaction);
+    }
 
 
 }
