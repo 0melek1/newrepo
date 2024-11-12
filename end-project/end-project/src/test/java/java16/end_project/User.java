8503415ff166c1e5c9d942482fed48b8eac0d05c
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-12 22:49:23 +0400

    transaction model class created

--- end-project/end-project/src/test/java/java16/end_project/User.java
+++ end-project/end-project/src/test/java/java16/end_project/User.java
@@ -1,4 +1,19 @@
 package java16.end_project;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
 }
