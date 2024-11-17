b9c6d2a0b4dec6fae8789eee523d0cf30bcac43d
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-17 22:01:17 +0400

    category controller created

---
+++ end-project/end-project/src/main/java/java16/end_project/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package java16.end_project.controller;
+
+import java16.end_project.entity.Category;
+import java16.end_project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.saveCategory(category));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
