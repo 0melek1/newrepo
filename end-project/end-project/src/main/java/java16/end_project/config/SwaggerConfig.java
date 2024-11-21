1d20757b37ef1c039acf9acca98f95b5a6580f03
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-21 23:48:53 +0400

    swagger config updated

--- end-project/end-project/src/main/java/java16/end_project/config/SwaggerConfig.java
+++ end-project/end-project/src/main/java/java16/end_project/config/SwaggerConfig.java
@@ -6,15 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
-public class SwaggerConfig {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("My API")
-                        .version("1.0")
-                        .description("API description"));
-    }
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Balance Management API",
+                version = "1.0",
+                description = "API for managing user balances",
+                contact = @Contact(
+                        name = "Mələk Hacıyeva",
+                        email = "melek.haciyeva@example.com"
+                )
+        ),
+        servers = @Server(
+                url = "http://localhost:8081",
+                description = "Development Server"
+        )
+)
+public class SwaggerConfig {
 }
 
