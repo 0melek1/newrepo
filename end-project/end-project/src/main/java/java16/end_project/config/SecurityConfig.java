c8c3da90c0be0421fbbe92507563d0c50f61d778
Melek <142055825+0melek1@users.noreply.github.com>
2024-11-21 23:47:29 +0400

    security config class updated

--- end-project/end-project/src/main/java/java16/end_project/config/SecurityConfig.java
+++ end-project/end-project/src/main/java/java16/end_project/config/SecurityConfig.java
@@ -2,34 +2,64 @@ package java16.end_project.config;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig  {
 
+
+    @Autowired
+    private DataSource dataSource;
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public UserDetailsService userDetailsService() {
+        JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
+        jdbcDao.setDataSource(dataSource);
+        return jdbcDao;
     }
-
-
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        return authProvider ;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/public/**").permitAll()
-                .anyRequest().authenticated()
+        return http.csrf().disable().authorizeHttpRequests().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers( "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/auth/**")
+                .permitAll()
+                .anyRequest().authenticated() // Digər bütün sorğular autentifikasiya tələb edir
                 .and()
-                .httpBasic(); // Example configuration
-        return http.build();
+                .httpBasic() // HTTP Basic Authentication aktivləşdirilir
+                .and()
+                .build();
+
     }
+
+
+
+
+
+
 }
